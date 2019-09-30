308ac3f874a2ef78a3eb02389921249a8d25654c
Artyzy_ <32649460+CamilePohlmann@users.noreply.github.com>
2019-09-30 16:49:30 -0300

    Alteração e nova tela 30/09

---
+++ application/views/comentario_view.php
@@ -0,0 +1,9 @@
+<body>
+	<div class="conteinerComent" id="sombra">
+		<h1 style="font-size: 30px; border-bottom: solid 2px #17a2b8; margin-bottom: 2%; padding-bottom: 1%;">Comentarios:</h1>
+		<div>
+			<figure></figure>
+			
+		</div>
+	</div>
+</body>
--- application/views/home.php
+++ application/views/home.php
@@ -93,7 +93,7 @@
                 <div>
                     <h3><?php echo $artigo->titulo?></h3>
                     <p><?php echo $artigo->resumo ?></p>
-                <label>Postado pelo aluno:<h4 id="nomeAutor2"><a href="<?php echo site_url('alunos/aluno_perfil')?>?codAluno=<?php echo $artigo->alunos_codAluno?>"><?php echo $artigo->nomeAluno;?></a></h4></label>
+                <label>Postado pelo aluno:<h4 id="nomeAutor2"><a href="<?php echo site_url('alunos/aluno_perfil')?>?codAluno=<?php echo $artigo->alunos_codAluno?>" style="color: #17a2b8;"><?php echo $artigo->nomeAluno;?></a></h4></label>
                 <br>
                     <a href="<?php echo site_url('artigos/artigo_page/')?>?codArtigo=<?php echo $artigo->codArtigo;?>" class="btn" id="visu">Visualizar</a>
                 </div>
@@ -112,13 +112,15 @@
                 <div>
                     <h3><?php echo $artigo->titulo?></h3>
                     <p><?php echo $artigo->resumo?></p>
-                        <label>Postado pelo professor:<h4 id="nomeAutor2"><a href="<?php echo site_url('professores/professor_perfil')?>?codProfessor=<?php echo $artigo->professores_codProfessor?>"><?php echo $artigo->nomeProfessor;?></a></h4></label>
+                        <label>Postado pelo professor:<h4 id="nomeAutor2"><a href="<?php echo site_url('professores/professor_perfil')?>?codProfessor=<?php echo $artigo->professores_codProfessor?>" style="color: #28a745;"><?php echo $artigo->nomeProfessor;?></a></h4></label>
                     <a href="<?php echo site_url('artigos/artigo_page/')?>?codArtigo=<?php echo $artigo->codArtigo;?>" class="btn" id="visu">Visualizar</a>
                 </div>
             </article>
         <br>
     <?php }?>
    </section>
+	<div class="espaco2"></div>
+	<?php include 'comentario_view.php' ?>
 	<div class="espaco2"></div>
 	
 
