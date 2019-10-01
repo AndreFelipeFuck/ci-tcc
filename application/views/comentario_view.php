09ac060afb851c8ce783c8193888d0f3ffc72040
Artyzy_ <32649460+CamilePohlmann@users.noreply.github.com>
2019-10-01 14:23:00 -0300

    Add files via upload

--- application/views/comentario_view.php
+++ application/views/comentario_view.php
@@ -1,9 +1,29 @@
+<?php include 'cabeca.php' ?>
+
 <body>
 	<div class="conteinerComent" id="sombra">
 		<h1 style="font-size: 30px; border-bottom: solid 2px #17a2b8; margin-bottom: 2%; padding-bottom: 1%;">Comentarios:</h1>
 		<div>
-			<figure></figure>
+			<section class="fotoPerfilComent">
+				<div>
+					<figure><img src="<?php echo base_url('assets/bootstrap/img/user.png')?>" class="img-fluid" alt="smaple image"></figure>
+				</div>
+			</section>
+			<div class="elementoComent">
+				<h5>Aluno Exemplo</h5>
+				<div>
+					<textarea style="height: 10%;"></textarea>
+				</div>
+			</div>
+			<div class="publicarComent">
+				<div>
+					<button class="btn" id="visu">Publicar</button>
+					<div style="height: 1%;"></div>
+					<button class="btn" id="perigo">Cancelar</button>
+				</div>
+			</div>
 			
 		</div>
 	</div>
-</body>
+<?php include 'rodape.php' ?>
+
