51247a24efdbc6fca290d7a8191174df1cd3c96a
AndreFelipeFuck <36815938+AndreFelipeFuck@users.noreply.github.com>
2019-10-02 13:59:14 -0300

    Agora vai

--- application/controllers/Artigos.php
+++ application/controllers/Artigos.php
@@ -53,20 +53,21 @@ class Artigos extends CI_Controller
 
 	public function artigo_add(){
 		$imgArtigo = $_FILES['imgArtigo'];
+		$pdfArtigo = $_FILES['uploadArtigo'];
 		//DANDO NOME AO ARQUIVO PDF
 			$titulo_pdf = explode(" ", $this->input->post('titulo'));
 			$titulo_pdf = implode("_", $titulo_pdf);
 			$titulo_pdf = $titulo_pdf."_".date("Y-m-d");
 		///
 		if($imgArtigo['name'] == null){
-			$config_pdf = array(
-	           	'upload_path' => './upload/pdf',
-	           	'allowed_types' => 'pdf',
-	           	'file_name' => $titulo_pdf.".pdf",
-	           	'max_size' => '3000'
-	           );
-	           $this->load->library('upload');
-	           $this->upload->initialize($config_pdf);
+			if ($pdfArtigo['name'] != null) {
+					$config_pdf = array(
+			           	'upload_path' => './upload/pdf',
+			           	'allowed_types' => 'pdf',
+			           	'file_name' => $titulo_pdf.".pdf",
+			           	'max_size' => '3000');
+	           		$this->load->library('upload');
+	           		$this->upload->initialize($config_pdf);
 
 	           if ($this->upload->do_upload('uploadArtigo')){
 	           		echo 'Arquivo salvo com sucesso.';
@@ -105,10 +106,52 @@ class Artigos extends CI_Controller
 		                $artigo = $query->row();
 		                $url = "?codArtigo=".$artigo->codArtigo;
 		               redirect ("artigos/artigo_page/$url");
+		               
 		            }
 
+		        }else{
+		         	echo $this->upload->display_errors();
 		        }
-		         echo $this->upload->display_errors();
+			}else{
+					echo 'Arquivo salvo com sucesso.';
+
+        			if($this->input->post('alunos_codAluno') == 0){
+						$data = array(
+							'titulo' => $this->input->post('titulo'),
+							'corpo' => $this->input->post('corpo'),
+							'professores_codProfessor' => $this->input->post('professores_codProfessor'),
+							'disciplina_codDisciplina' => $this->input->post('disciplina_codDisciplina'),
+							'resumo' => $this->input->post('resumo'),
+							'dataArtigo' => date("Y-m-d"),
+						);
+
+					}if ($this->input->post('professores_codProfessor') == 0) {
+						$data = array(
+								'titulo' => $this->input->post('titulo'),
+								'corpo' => $this->input->post('corpo'),
+								'alunos_codAluno' => $this->input->post('alunos_codAluno'),
+								'disciplina_codDisciplina' => $this->input->post('disciplina_codDisciplina'),
+								'resumo' => $this->input->post('resumo'),
+								'dataArtigo' => date("Y-m-d"),
+						);
+					}
+
+					$insert = $this->artigos_model->artigo_add($data);
+
+					$titulo = $this->db->where('titulo', $data['titulo']);
+		           	$corpo = $this->db->where('corpo', $data['corpo']);
+		            $query = $this->db->get('artigos');
+					$codArtigo = $this->artigos_model->get_by_login($titulo, $corpo);
+				    //Defenindo a disciplina do artigo	
+		            if ($query->num_rows() == 1){
+		                $artigo = $query->row();
+		                $url = "?codArtigo=".$artigo->codArtigo;
+		               //redirect ("artigos/artigo_page/$url");
+		                print_r($_FILES);
+		            }
+
+			}//
+			
 		        /*echo "Deu errado ";
 		        echo "$titulo_pdf";
 		        print_r($_FILES);*/
