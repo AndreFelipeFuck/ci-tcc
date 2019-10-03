3b8900ff7d1594f1baf50a51e9ed1cdc8459bc35
Artyzy_ <32649460+CamilePohlmann@users.noreply.github.com>
2019-10-03 14:24:07 -0300

    novo layout sobre

--- application/views/sobre_view.php
+++ application/views/sobre_view.php
@@ -3,6 +3,36 @@
 	<title>Atom | Sobre</title>
 </head>
 <body>
+	<div class="espaco2"></div>
+	<div class="conteinerSobre" id="sombra">
+		<div>
+			<h1 style="font-size: 35px; border-bottom: solid 2px #17a2b8; margin-bottom: 2%; padding-bottom: 1%;">O que é o Atom?</h1>
+			<h6>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h6>
+		</div>
+		<div style="width: 90%; border-bottom: solid 2px #17a2b8; display: inline-block; margin-left: 5%; margin-right: 5%; margin-top: 2%; margin-bottom: 3%; opacity: 0.3;"></div>
+		<div>
+			<h1 style="font-size: 35px; border-bottom: solid 2px #17a2b8; margin-bottom: 2%; padding-bottom: 1%;">Origem do projeto</h1>
+			<h6>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h6>
+		</div>
+		<div style="width: 90%; border-bottom: solid 2px #17a2b8; display: inline-block; margin-left: 5%; margin-right: 5%; margin-top: 2%; margin-bottom: 3%; opacity: 0.3;"></div>
+		<div style="display: inline-block; width: 100%;">
+			<h1 style="font-size: 35px; border-bottom: solid 2px #17a2b8; margin-bottom: 2%; padding-bottom: 1%;">Desenvolvedores</h1>
+			<div class="espaco2"></div>
+			<div class="contPort">
+				<figure id="detPort"><img src="<?php echo base_url('assets//bootstrap/img/bio.png')?>"></figure>
+				<h3 id="detPortFont">Desenvolvedor</h3>
+				<h5 id="detPortNome">jesus michel tornavalt matilda fuck</h5>
+			</div>
+			<div class="contPort2">
+				<figure id="detPort"><img src="<?php echo base_url('assets//bootstrap/img/bio.png')?>"></figure>
+				<h3 id="detPortFont">Desenvolvedor</h3>
+			</div>
+			<div class="contPort3">
+				<figure id="detPort"><img src="<?php echo base_url('assets//bootstrap/img/bio.png')?>"></figure>
+				<h3 id="detPortFont">Desenvolvedor</h3>
+			</div>
+		</div>
+	</div>
 
 
 <?php include 'rodape.php' ?>
